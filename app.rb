f4123b6a4541de18c001b55abc5d62726fe7b5b1
tknzk <tkm.knzk@gmail.com>
2013-05-09 14:33:29 +0900

    add newrelic config

--- app.rb
+++ app.rb
@@ -4,14 +4,18 @@ require 'feed-normalizer'
 require 'sinatra'
 require 'open-uri'
 
+configure :production do
+    require 'newrelic_rpm'
+end
+
 get '/' do
 
     uri_blog = 'http://tknzk.hatenablog.jp/feed'
     uri_tech = 'http://tknzk.hateblo.jp/feed'
-    
+
     feed_blog = FeedNormalizer::FeedNormalizer.parse open(uri_blog)
     feed_tech = FeedNormalizer::FeedNormalizer.parse open(uri_tech)
-    
+
     #feed.entries.each{ |e|
     #    puts e.url
     #    puts e.title
