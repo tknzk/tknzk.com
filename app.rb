9eb6a913f1e7176385cf2dc7adc110115c465d19
tknzk <tkm.knzk@gmail.com>
2013-07-19 10:34:06 +0900

    nest

--- app.rb
+++ app.rb
@@ -8,24 +8,24 @@ require 'newrelic_rpm'
 
 get '/' do
 
-    uri_blog = 'http://tknzk.hatenablog.jp/feed'
-    uri_tech = 'http://tknzk.hateblo.jp/feed'
+  uri_blog = 'http://tknzk.hatenablog.jp/feed'
+  uri_tech = 'http://tknzk.hateblo.jp/feed'
 
-    feed_blog = FeedNormalizer::FeedNormalizer.parse open(uri_blog)
-    feed_tech = FeedNormalizer::FeedNormalizer.parse open(uri_tech)
+  feed_blog = FeedNormalizer::FeedNormalizer.parse open(uri_blog)
+  feed_tech = FeedNormalizer::FeedNormalizer.parse open(uri_tech)
 
-    #feed.entries.each{ |e|
-    #    puts e.url
-    #    puts e.title
-    #    puts e.last_updated
-    #    puts ''
-    #}
+  #feed.entries.each{ |e|
+  #    puts e.url
+  #    puts e.title
+  #    puts e.last_updated
+  #    puts ''
+  #}
 
-    erb :index,
-        :locals => {
-            :feed_blog => feed_blog,
-            :feed_tech => feed_tech
-            }
+  erb :index,
+    :locals => {
+      :feed_blog => feed_blog,
+      :feed_tech => feed_tech
+  }
 
 end
 
