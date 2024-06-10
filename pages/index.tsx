966b6a47919e5033b942e6e1ec3f226752e63872
tknzk <tkm.knzk@gmail.com>
2024-06-10 15:18:41 +0900

    add

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+// pages/index.tsx
+import { NextPage } from 'next';
+
+const Home: NextPage = () => {
+    return (
+        <div>
+            <h1>Hello, TypeScript with Next.js on Google App Engine!</h1>
+        </div>
+    );
+};
+
+export default Home;
